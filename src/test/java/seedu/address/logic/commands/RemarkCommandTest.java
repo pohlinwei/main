76df88a1efb49f74d13d8d5cabe38e9500a25a56
Lin Wei <linwei@Lins-MacBook-Pro-2.local>
2019-09-28 12:23:28 +0800

    Add remark command

---
+++ src/test/java/seedu/address/logic/commands/RemarkCommandTest.java
@@ -0,0 +1,27 @@
+package seedu.address.logic.commands;
+
+import org.junit.jupiter.api.Test;
+
+import static seedu.address.logic.commands.CommandTestUtil.assertCommandFailure;
+import static seedu.address.logic.commands.RemarkCommand.MESSAGE_ARGUMENTS;
+import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
+import static seedu.address.logic.commands.CommandTestUtil.showPersonAtIndex;
+import static seedu.address.testutil.TypicalIndexes.INDEX_FIRST_PERSON;
+import static seedu.address.testutil.TypicalIndexes.INDEX_SECOND_PERSON;
+import static seedu.address.testutil.TypicalPersons.getTypicalAddressBook;
+import seedu.address.commons.core.Messages;
+import seedu.address.commons.core.index.Index;
+import seedu.address.model.AddressBook;
+import seedu.address.model.Model;
+import seedu.address.model.ModelManager;
+import seedu.address.model.UserPrefs;
+import seedu.address.model.person.Person;
+import seedu.address.model.person.Remark;
+import seedu.address.testutil.PersonBuilder;
+
+class RemarkCommandTest {
+
+    @Test
+    void execute() {
+    }
+}
